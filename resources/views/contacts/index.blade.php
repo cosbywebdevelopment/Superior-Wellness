75819f2339e0e4b8f2b5e419d558a590fdbf76e4
cosbywebdevelopment <nicknexus1973@gmail.com>
2023-06-29 11:06:03 +0100

    Tidy up

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,13 +1,8 @@
 @extends('layouts.app')
 @section('title', 'Business Contacts')
 
-
 @section('content')
     <p>Lists all of your businesses Contacts</p>
-
-    <p>Edit page will have 3 address fields with a checkbox to inform if shipping or delivery address (unique
-        field).</p>
-
     <table class="table">
         <thead>
         <tr>
@@ -23,14 +18,10 @@
                 <td>{{ $contact->contact_name }}</td>
                 <td>
                     <a href="{{route('contacts.show',$contact->id)}}" class="btn btn-info btn-sm">View</a>
-{{--                    <a href="{{route('contacts.edit',$contact->id)}}" class="btn btn-warning btn-sm">Edit</a>--}}
-{{--                    <a href="" class="btn btn-danger">Delete</a>--}}
                 </td>
             </tr>
         @endforeach
         </tbody>
     </table>
-
-
 @endsection
 
